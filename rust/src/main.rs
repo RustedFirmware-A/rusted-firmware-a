af6d831a684de08abf6acf45616ba79797defa50
Andrew Walbran <qwandor@google.com>
2024-01-30 16:05:10 +0000

    feat(rust): log parameters to main function
    Change-Id: I71f9c12e719d0961ab8680ad4299394c7dce6ed2
    Signed-off-by: Andrew Walbran <qwandor@google.com>

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -15,11 +15,12 @@ use log::{info, LevelFilter};
 const PL011_BASE_ADDRESS: *mut u32 = 0x900_0000 as _;
 
 #[no_mangle]
-extern "C" fn bl31_main() {
+extern "C" fn bl31_main(bl31_params: u64, platform_params: u64) {
     // Safe because `PL011_BASE_ADDRESS` is the base address of a PL011 device,
     // and nothing else accesses that address range.
     let uart = unsafe { Uart::new(PL011_BASE_ADDRESS) };
     logger::init(uart, LevelFilter::Trace).unwrap();
     info!("Rust BL31 starting");
+    info!("Parameters: {:#0x} {:#0x}", bl31_params, platform_params);
     loop {}
 }

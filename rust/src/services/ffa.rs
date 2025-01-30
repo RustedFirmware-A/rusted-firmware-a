cbb86558987edfd64f13bf13800219354849b35c
Tomás González <tomasagustin.gonzalezorlando@arm.com>
2025-01-30 17:22:50 +0000

    feat(rust): use arm-ffa crate
    Increase the LENGTH in qemu.ld so that the image fits even when
    building with DEBUG=1.
    
    Change-Id: I84c14278e77f094ce12212daaa72e1633d473858
    Signed-off-by: Tomás González <tomasagustin.gonzalezorlando@arm.com>

--- rust/src/services/ffa.rs
+++ rust/src/services/ffa.rs
@@ -2,24 +2,20 @@
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
-use log::info;
+use arm_ffa::{FfaError, FuncId, Interface, Version};
+use log::{error, info};
 
 use crate::{
     context::{switch_world, World},
     services::{owns, Service},
-    smccc::{FunctionId, OwningEntityNumber, SmcReturn, NOT_SUPPORTED},
+    smccc::{FunctionId, OwningEntityNumber, SmcReturn},
 };
 
 const FUNCTION_NUMBER_MIN: u16 = 0x0060;
 const FUNCTION_NUMBER_MAX: u16 = 0x00FF;
 
-const FFA_ERROR: u32 = 0x8400_0060;
-const FFA_SUCCESS: u32 = 0x8400_0061;
-const FFA_SUCCESS64: u32 = 0xC400_0061;
-const FFA_VERSION: u32 = 0x8400_0063;
-const FFA_MSG_WAIT: u32 = 0x8400_006B;
-
-const FFA_VERSION_1_1: u32 = 1 << 16 | 1;
+const FFA_VERSION_1_0: Version = Version(1, 0);
+const FFA_VERSION_1_1: Version = Version(1, 1);
 
 /// Arm Firmware Framework for A-Profile.
 pub struct Ffa;
@@ -34,24 +30,42 @@ impl Service for Ffa {
         function: FunctionId,
         x1: u64,
         x2: u64,
-        _x3: u64,
-        _x4: u64,
+        x3: u64,
+        x4: u64,
         world: World,
     ) -> SmcReturn {
-        match function.0 {
-            FFA_VERSION => version(world, x1 as u32).into(),
-            FFA_MSG_WAIT => msg_wait(world, x2 as u32),
-            _ => NOT_SUPPORTED.into(),
+        let regs = [function.0.into(), x1, x2, x3, x4, 0, 0, 0];
+
+        let msg = match Interface::from_regs(FFA_VERSION_1_1, &regs) {
+            Ok(msg) => msg,
+            Err(e) => {
+                error!("Invalid FF-A call {:#x?}", e);
+                return Interface::error(e.into()).into();
+            }
+        };
+        match msg {
+            Interface::Version { input_version } => version(world, input_version),
+            Interface::MsgWait => msg_wait(world, 0),
+            _ => Interface::error(FfaError::NotSupported),
         }
+        .into()
     }
 }
 
 /// Returns the version of the Firmware Framework implementation supported by RF-A.
-fn version(_world: World, _input_version: u32) -> u32 {
-    FFA_VERSION_1_1
+fn version(world: World, _input_version: Version) -> Interface {
+    match world {
+        // TODO: Implement this properly (Direct Message if needed)
+        World::NonSecure => Interface::VersionOut {
+            output_version: FFA_VERSION_1_0,
+        },
+        World::Secure => Interface::VersionOut {
+            output_version: FFA_VERSION_1_1,
+        },
+    }
 }
 
-fn msg_wait(world: World, _flags: u32) -> SmcReturn {
+fn msg_wait(world: World, _flags: u32) -> Interface {
     match world {
         World::Secure => {
             // TODO: Check flags and possibly update ownership of RX buffer.
@@ -62,33 +76,25 @@ fn msg_wait(world: World, _flags: u32) -> SmcReturn {
             // return value before switching back to secure world. This return value will only be
             // seen by secure world if there is a bug where we fail to write an appropriate return
             // value when next we switch to secure world, so make it an error code we can recognise.
-            FfaError::Denied.into()
+            Interface::error(FfaError::Denied)
         }
         _ => {
             // FFA_MSG_WAIT is not allowed over SMC in the non-secure physical instance.
-            FfaError::NotSupported.into()
+            Interface::error(FfaError::NotSupported)
         }
     }
 }
 
-/// An error returned from an FF-A function call.
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-#[repr(i32)]
-enum FfaError {
-    NotSupported = -1,
-    InvalidParameters = -2,
-    NoMemory = -3,
-    Busy = -4,
-    Interrupted = -5,
-    Denied = -6,
-    Retry = -7,
-    Aborted = -8,
-    NoData = -9,
-    NotReady = -10,
-}
-
 impl From<FfaError> for SmcReturn {
     fn from(value: FfaError) -> Self {
-        [FFA_ERROR.into(), 0, value as u64].into()
+        [FuncId::Error as u64, 0, value as u64].into()
+    }
+}
+
+impl From<Interface> for SmcReturn {
+    fn from(interface: Interface) -> Self {
+        let mut smc_return: [u64; 8] = [0; 8];
+        interface.to_regs(FFA_VERSION_1_1, &mut smc_return);
+        SmcReturn::from(smc_return)
     }
 }

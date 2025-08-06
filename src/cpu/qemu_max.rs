8fdb248a06b2b61429e9fbf29c37a25e460b4594
Jeremi Miller <jeremimiller@google.com>
2025-08-06 17:31:19 +0200

    feat(rust): add Qemu Max CPU
    Change-Id: Ie208ac92ebf271e285f470732c6acc0d565d9664
    Signed-off-by: Jeremi Miller <jeremimiller@google.com>

---
+++ src/cpu/qemu_max.rs
@@ -0,0 +1,22 @@
+// Copyright The Rusted Firmware-A Contributors.
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+use super::Cpu;
+use core::arch::naked_asm;
+
+pub struct QemuMax;
+
+/// Safety: The reset handler is implemented as a naked function and does not clobber any registers.
+unsafe impl Cpu for QemuMax {
+    const MIDR: u64 = 0x000f_0510;
+
+    #[unsafe(naked)]
+    extern "C" fn reset_handler() {
+        naked_asm!("ret");
+    }
+
+    fn power_down_level0() {}
+
+    fn power_down_level1() {}
+}

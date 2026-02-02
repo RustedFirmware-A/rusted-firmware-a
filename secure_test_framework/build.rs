RustedFirmware-A/rusted-firmware-a

commit ee7828114438cb2108c8c35065e7f019831e99d5

feat(rme): handle warmboots context for Realm World
Initializes registers for entering Realm World during warm boot
according to TF-A's documentation.

Change-Id: I750f5c175fa5f2d75759320bd0b1ddf5859af76e
Signed-off-by: Ludovic Mermod <ludovic.mermod@arm.com>

secure_test_framework/src/stf_rmm.rs
@@ -21,7 +21,10 @@ mod platform;
 mod tests;
 mod util;
 
-use core::panic::PanicInfo;
+use core::{
+    panic::PanicInfo,
+    sync::atomic::{AtomicBool, Ordering},
+};
 
 use aarch64_rt::{enable_mmu, entry};
 use log::{error, info};
@@ -32,29 +35,143 @@ use crate::{
     util::current_el,
 };
 
+const SUPPORTED_RMM_VERSION: RmmBootManifestVersion = RmmBootManifestVersion { major: 0, minor: 8 };
+const SUPPORTED_RMM_MANIFEST_VERSION: RmmBootManifestVersion =
+    RmmBootManifestVersion { major: 0, minor: 5 };
+
 const RMM_BOOT_COMPLETE: u32 = 0xC400_01CF;
 
 enable_mmu!(RMM_IDMAP);
 
-entry!(realm_main, 4);
-fn realm_main(x0: u64, x1: u64, x2: u64, x3: u64) -> ! {
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct RmmBootManifestVersion {
+    pub(crate) major: u16,
+    pub(crate) minor: u16,
+}
+
+impl TryFrom<u32> for RmmBootManifestVersion {
+    type Error = ();
+
+    fn try_from(value: u32) -> Result<Self, Self::Error> {
+        let major = (value >> 16) as u16;
+
+        if major & 0x7fff != major {
+            return Err(());
+        }
+
+        Ok(Self {
+            major,
+            minor: (value & 0xFFFF) as u16,
+        })
+    }
+}
+
+impl From<RmmBootManifestVersion> for u32 {
+    fn from(value: RmmBootManifestVersion) -> Self {
+        (value.major as u32) << 16 | value.minor as u32
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+#[allow(missing_docs)]
+pub enum RmmBootReturn {
+    Success = 0,
+    Unknown = -1,
+    VersionNotValid = -2,
+    CpusOutOfRange = -3,
+    CpuIdOutOfRange = -4,
+    InvalidSharedBuffer = -5,
+    ManifestVersionNotSupported = -6,
+    ManifestDataError = -7,
+}
+
+fn complete_boot(ret: RmmBootReturn, extra_args: &[u64]) -> ! {
+    assert!(extra_args.len() <= 16);
+
+    // Sends a RMM_BOOT_COMPLETE SMC to notify the Root World that RMM has booted.
+    let mut args: [u64; 17] = [0; 17];
+    args[1..1 + extra_args.len()].copy_from_slice(extra_args);
+
+    args[0] = ret as u64;
+    smc64(RMM_BOOT_COMPLETE, args);
+
+    // TODO: handle RMI calls originating from NS world.
+    todo!()
+}
+
+fn validate_coldboot_args(pe_idx: u64, version: u64, core_count: u64, _shared_buffer_addr: u64) {
+    if pe_idx >= core_count {
+        complete_boot(RmmBootReturn::CpuIdOutOfRange, &[]);
+    }
+
+    let Ok(version) = RmmBootManifestVersion::try_from(version as u32) else {
+        complete_boot(RmmBootReturn::VersionNotValid, &[])
+    };
+
+    if version.major != SUPPORTED_RMM_VERSION.major {
+        complete_boot(RmmBootReturn::VersionNotValid, &[])
+    }
+
+    // TODO: check shared buffer
+
+    if core_count > PlatformImpl::CORE_COUNT as u64 {
+        complete_boot(RmmBootReturn::CpusOutOfRange, &[]);
+    }
+}
+
+/// Indicates whether the image already went through a cold boot.
+///
+/// TODO: due to a bug likely in [`entry!`], the `.bss` segment is reinitialized each time the
+/// machine jumps to the entrypoint, hence overriding any changes done during coldboot. This
+/// variable is set to `true` to be stored in the `.data` segment, avoiding the issue.
+static NEEDS_COLD_BOOT: AtomicBool = AtomicBool::new(true);
+
+entry!(rmm_main, 4);
+fn rmm_main(x0: u64, x1: u64, x2: u64, x3: u64) -> ! {
+    if !NEEDS_COLD_BOOT.load(Ordering::Acquire) {
+        warmboot_main(x0, x1)
+    } else {
+        NEEDS_COLD_BOOT.store(false, Ordering::Release);
+        coldboot_main(x0, x1, x2, x3)
+    }
+}
+
+fn coldboot_main(pe_idx: u64, version: u64, core_count: u64, shared_buffer_addr: u64) -> ! {
+    validate_coldboot_args(pe_idx, version, core_count, shared_buffer_addr);
+
     let log_sink = PlatformImpl::make_log_sink();
     logger::init(log_sink).unwrap();
 
     info!(
         "Fake RMM starting at EL {} with args {:#x}, {:#x}, {:#x}, {:#x}",
         current_el(),
-        x0,
-        x1,
-        x2,
-        x3,
+        pe_idx,
+        version,
+        core_count,
+        shared_buffer_addr,
     );
 
-    let args = [0; 17];
-    smc64(RMM_BOOT_COMPLETE, args);
+    complete_boot(RmmBootReturn::Success, &[0xdead_beef])
+}
 
-    // The `smc` instruction in the previous asm section never returns.
-    unreachable!()
+fn warmboot_main(pe_idx: u64, activation_token: u64) -> ! {
+    let log_sink = PlatformImpl::make_log_sink();
+    logger::init(log_sink).unwrap();
+
+    info!(
+        "Fake RMM warmboot at EL {} with args {:#x}, {:#x}",
+        current_el(),
+        pe_idx,
+        activation_token,
+    );
+
+    let generated_activation_token = if activation_token == 0 {
+        0xdead_beef
+    } else {
+        0
+    };
+
+    complete_boot(RmmBootReturn::Success, &[generated_activation_token])
 }
 
 #[panic_handler]

src/context.rs
@@ -936,7 +936,11 @@ fn initialise_realm(context: &mut CpuContext, entry_point: &EntryPointInfo) {
 /// When the CPU wakes up from a powerdown suspend state, lower ELs in each world expect a specific
 /// state for resuming their execution. This can be a different entry point or just arguments passed
 /// in registers.
-pub fn update_contexts_suspend(psci_entrypoint: EntryPoint, secure_args: &SmcReturn) {
+pub fn update_contexts_suspend(
+    psci_entrypoint: EntryPoint,
+    secure_args: &SmcReturn,
+    #[cfg(feature = "rme")] realm_args: &[u64],
+) {
     initialise_el3_sysregs();
 
     exception_free(|token| {
@@ -955,7 +959,8 @@ pub fn update_contexts_suspend(psci_entrypoint: EntryPoint, secure_args: &SmcRet
 
         cpu_state[World::Secure].gpregs.registers[..18].copy_from_slice(secure_args.values());
 
-        // TODO: implement suspend handling for Realm
+        #[cfg(feature = "rme")]
+        cpu_state[World::Realm].gpregs.registers[..realm_args.len()].copy_from_slice(realm_args);
     });
 }
 

src/main.rs
@@ -124,9 +124,17 @@ extern "C" fn psci_warmboot_entrypoint() -> ! {
 
             let secure_args = services.spmd.handle_wake_from_cpu_suspend();
 
+            #[cfg(feature = "rme")]
+            let realm_args = services.rmmd.handle_wake_from_cpu_suspend();
+
             // TODO: instead of modifying the context directly, should we rather pass the initial
             // gpregs of each world as arguments to run_loop()?
-            update_contexts_suspend(psci_entrypoint, &secure_args);
+            update_contexts_suspend(
+                psci_entrypoint,
+                &secure_args,
+                #[cfg(feature = "rme")]
+                &realm_args,
+            );
         }
     }
 

src/services/rmmd.rs
@@ -2,12 +2,15 @@
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
-use percore::Cores;
+use core::cell::RefCell;
+
+use percore::{Cores, ExceptionLock, PerCore};
+use spin::Once;
 
 use crate::{
-    context::{CoresImpl, World},
+    context::{CoresImpl, PerCoreState, World},
     info,
-    platform::{Platform, PlatformImpl},
+    platform::{Platform, PlatformImpl, exception_free},
     services::{Service, owns},
     smccc::{FunctionId, NOT_SUPPORTED, OwningEntityNumber, SetFrom, SmcReturn},
 };
@@ -15,11 +18,28 @@ use crate::{
 const RMM_BOOT_VERSION: u64 = 0x5;
 const RMM_BOOT_COMPLETE: u32 = 0xC400_01CF;
 
+#[derive(Debug)]
+struct RmmdLocal {
+    activation_token: Option<u64>,
+}
+
+impl RmmdLocal {
+    const fn new() -> Self {
+        Self {
+            activation_token: None,
+        }
+    }
+}
+
+pub static RMM_COLD_BOOT_DONE: Once<()> = Once::new();
+
 /// Arm CCA SMCs, for communication between RF-A and TF-RMM.
 ///
 /// This is described at
 /// <https://trustedfirmware-a.readthedocs.io/en/latest/components/rmm-el3-comms-spec.html>
-pub struct Rmmd;
+pub struct Rmmd {
+    core_local: PerCoreState<RmmdLocal>,
+}
 
 impl Service for Rmmd {
     owns! {OwningEntityNumber::STANDARD_SECURE, 0x0150..=0x01CF}
@@ -44,26 +64,74 @@ impl Service for Rmmd {
 
 impl Rmmd {
     pub(super) fn new() -> Self {
-        Self
+        let core_local = PerCore::new(
+            [const { ExceptionLock::new(RefCell::new(RmmdLocal::new())) };
+                PlatformImpl::CORE_COUNT],
+        );
+
+        Self { core_local }
+    }
+
+    /// Initializes the set of registers to pass to R-EL2 after waking up from a suspend.
+    ///
+    /// <https://trustedfirmware-a.readthedocs.io/en/latest/components/rmm-el3-comms-spec.html#warm-boot-interface>
+    pub(crate) fn handle_wake_from_cpu_suspend(&self) -> [u64; 4] {
+        let activation_token = exception_free(|token| {
+            self.core_local
+                .get()
+                .borrow(token)
+                .borrow()
+                .activation_token
+                .unwrap_or_default()
+        });
+
+        [CoresImpl::core_index() as u64, activation_token, 0, 0]
     }
 
     fn handle_boot_complete(&self, regs: &mut SmcReturn) -> World {
         let ret = regs.values()[1] as i32;
+
+        if ret != 0 {
+            panic!("RMM Boot failed (code: {ret})")
+        }
+
+        exception_free(|token| {
+            let mut state = self.core_local.get().borrow_mut(token);
+
+            if state.activation_token.is_none() {
+                let activation_token = regs.values()[2];
+                info!("Received activation token {activation_token:#x?}");
+                state.activation_token = Some(activation_token)
+            }
+        });
+
+        RMM_COLD_BOOT_DONE.call_once(|| ());
         regs.set_from(ret);
+
         World::NonSecure
     }
 
     pub fn entrypoint_args(&self) -> [u64; 8] {
         let core_linear_id = CoresImpl::core_index() as u64;
-        [
-            core_linear_id,
-            RMM_BOOT_VERSION,
-            PlatformImpl::CORE_COUNT as u64,
-            PlatformImpl::RMM_SHARED_BUFFER_START as u64,
-            0,
-            0,
-            0,
-            0,
-        ]
+        if RMM_COLD_BOOT_DONE.is_completed() {
+            // When warmbooting a PE for the first time, it should only receive the core id as
+            // per the RMM-EL3 warmboot interface. Activation token is set to 0 as it was not
+            // generated for this core yet. Subsequent warmboot parameters on this PE will be
+            // provided by [`Rmmd::handle_wake_from_cpu_suspend`].
+            //
+            // https://trustedfirmware-a.readthedocs.io/en/latest/components/rmm-el3-comms-spec.html#warm-boot-interface
+            [core_linear_id, 0, 0, 0, 0, 0, 0, 0]
+        } else {
+            [
+                core_linear_id,
+                RMM_BOOT_VERSION,
+                PlatformImpl::CORE_COUNT as u64,
+                PlatformImpl::RMM_SHARED_BUFFER_START as u64,
+                0,
+                0,
+                0,
+                0,
+            ]
+        }
     }
 }

commit 23b6299aaff54126981a6dd05573001877f7eeae

fix(rme): use dedicated entrypoint for stf_rmm image
Use a custom entrypoint to determine whether the PE must perform a cold
or warm boot.

Fixes: ee78281 ("feat(rme): handle warmboots context for Realm World")
Change-Id: I3e5a7aad16d1c194b45d06d64ec6a93dac830863
Signed-off-by: Ludovic Mermod <ludovic.mermod@arm.com>

secure_test_framework/build.rs
@@ -15,12 +15,15 @@ fn main() {
         "cargo::rustc-check-cfg=cfg(platform, values(\"{}\"))",
         PLATFORMS.join("\", \""),
     );
-    println!("cargo:rustc-link-arg=-Timage.ld");
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
     println!("cargo:rustc-link-arg-bin=bl32=-T{crate_dir}/{platform}_bl32.ld");
-    println!("cargo:rustc-link-arg-bin=bl33=-T{crate_dir}/{platform}_bl33.ld");
     println!("cargo:rerun-if-changed={crate_dir}/{platform}_bl32.ld");
+    println!("cargo:rustc-link-arg-bin=bl32=-Timage.ld");
+
+    println!("cargo:rustc-link-arg-bin=bl33=-T{crate_dir}/{platform}_bl33.ld");
     println!("cargo:rerun-if-changed={crate_dir}/{platform}_bl33.ld");
+    println!("cargo:rustc-link-arg-bin=bl33=-Timage.ld");
 
     #[cfg(feature = "rme")]
     {

secure_test_framework/fvp_stf_rmm.ld
@@ -8,3 +8,85 @@ MEMORY
 {
 	image : ORIGIN = 0xfdc00000, LENGTH = 0x2000000
 }
+
+ENTRY(entrypoint)
+
+SECTIONS
+{
+	/*
+	 * Collect together the code.
+	 */
+	.init : ALIGN(4096) {
+		text_begin = .;
+		*(.init.entrypoint)
+		*(.init.*)
+	} >image
+	.text : {
+		*(.text.*)
+	} >image
+	text_end = .;
+
+	/*
+	 * Collect together read-only data.
+	 */
+	.rodata : ALIGN(4096) {
+		rodata_begin = .;
+		*(.rodata.*)
+	} >image
+	.got : {
+		*(.got)
+	} >image
+	rodata_end = .;
+
+	/*
+	 * Collect together the read-write data including .bss at the end which
+	 * will be zero'd by the entry code.
+	 */
+	.data : ALIGN(4096) {
+		data_begin = .;
+		*(.data.*)
+		/*
+		 * The entry point code assumes that .data is a multiple of 32
+		 * bytes long.
+		 */
+		. = ALIGN(32);
+		data_end = .;
+	} >image
+
+	/* Everything beyond this point will not be included in the binary. */
+	bin_end = .;
+
+	/* The entry point code assumes that .bss is 16-byte aligned. */
+	.bss : ALIGN(16)  {
+		bss_begin = .;
+		*(.bss.*)
+		*(COMMON)
+		. = ALIGN(16);
+		bss_end = .;
+	} >image
+
+	.stack (NOLOAD) : ALIGN(4096) {
+		boot_stack_begin = .;
+		KEEP(*(.stack.boot_stack))
+		. = ALIGN(4096);
+		boot_stack_end = .;
+
+		KEEP(*(.stack.*))
+	} >image
+
+	. = ALIGN(4K);
+	PROVIDE(dma_region = .);
+
+	/*
+	 * Remove unused sections from the image.
+	 */
+	/DISCARD/ : {
+		/* The image loads itself so doesn't need these sections. */
+		*(.gnu.hash)
+		*(.hash)
+		*(.interp)
+		*(.eh_frame_hdr)
+		*(.eh_frame)
+		*(.note.gnu.build-id)
+	}
+}

secure_test_framework/src/stf_rmm.rs
@@ -18,21 +18,19 @@ mod heap;
 mod logger;
 mod pagetable;
 mod platform;
+mod secondary;
 mod tests;
 mod util;
 
-use core::{
-    panic::PanicInfo,
-    slice::from_raw_parts_mut,
-    sync::atomic::{AtomicBool, Ordering},
-};
+use core::{arch::naked_asm, panic::PanicInfo, ptr::read};
 
-use aarch64_rt::{enable_mmu, entry};
+use aarch64_rt::{enable_mmu, entry, set_exception_vector};
 use log::{error, info};
 use smccc::{psci, smc64};
 
 use crate::{
     platform::{Platform, PlatformImpl, RMM_IDMAP},
+    secondary::secondary_entry,
     util::current_el,
 };
 
@@ -235,22 +233,37 @@ fn validate_coldboot_args(pe_idx: u64, version: u64, core_count: u64, shared_buf
 
 /// Indicates whether the image already went through a cold boot.
 ///
-/// TODO: due to a bug likely in [`entry!`], the `.bss` segment is reinitialized each time the
-/// machine jumps to the entrypoint, hence overriding any changes done during coldboot. This
-/// variable is set to `true` to be stored in the `.data` segment, avoiding the issue.
-static NEEDS_COLD_BOOT: AtomicBool = AtomicBool::new(true);
+/// This variable must only be accessed by [`entrypoint`] while MMU and caches are disabled.
+static mut NEEDS_COLD_BOOT: bool = true;
 
-entry!(rmm_main, 4);
-fn rmm_main(x0: u64, x1: u64, x2: u64, x3: u64) -> ! {
-    if !NEEDS_COLD_BOOT.load(Ordering::Acquire) {
-        warmboot_main(x0, x1)
-    } else {
-        NEEDS_COLD_BOOT.store(false, Ordering::Release);
-        coldboot_main(x0, x1, x2, x3)
-    }
+#[unsafe(naked)]
+#[unsafe(link_section = ".init.entrypoint")]
+#[unsafe(export_name = "entrypoint")]
+unsafe extern "C" fn entrypoint() -> ! {
+    naked_asm!(
+        // Fetches NEEDS_COLD_BOOT.
+        "adrp x30, {marker}",
+        "add x30, x30, :lo12:{marker}",
+        "ldrb w29, [x30]",
+        "cmp w29, #0",
+        "b.eq 1f",
+
+        // If NEEDS_COLD_BOOT == true, set it to false and go to aarch64_rt::entry.
+        "mov w29, #0",
+        "strb w29, [x30]",
+        "b entry",
+
+        // Otherwise jump to [`crate::secondary::secondary_entry`].
+        "1:",
+        "b {warmboot_main}",
+        marker = sym NEEDS_COLD_BOOT,
+        warmboot_main = sym secondary_entry,
+    )
 }
 
-fn coldboot_main(pe_idx: u64, version: u64, core_count: u64, shared_buffer_addr: u64) -> ! {
+entry!(rmm_main, 4);
+
+fn rmm_main(pe_idx: u64, version: u64, core_count: u64, shared_buffer_addr: u64) -> ! {
     validate_coldboot_args(pe_idx, version, core_count, shared_buffer_addr);
 
     let log_sink = PlatformImpl::make_log_sink();
@@ -266,11 +279,11 @@ fn coldboot_main(pe_idx: u64, version: u64, core_count: u64, shared_buffer_addr:
     );
 
     // Safety: the specification states that the `x3` register of the RMM is a pointer to a 4KB
-    // page mapped into the Realm World.
-    let shared_buf =
-        unsafe { from_raw_parts_mut(shared_buffer_addr as *mut u32, 0x1000 / size_of::<u32>()) };
+    // page mapped into the Realm World. Since this is coldboot, no other PE is running hence no
+    // concurrent write can occur.
+    let manifest_version = unsafe { read(shared_buffer_addr as *mut u32) };
 
-    let Ok(manifest_version) = RmmBootManifestVersion::try_from(shared_buf[0]) else {
+    let Ok(manifest_version) = RmmBootManifestVersion::try_from(manifest_version) else {
         complete_boot(RmmBootReturn::VersionNotValid, &[]);
         unreachable!()
     };
@@ -289,9 +302,8 @@ fn coldboot_main(pe_idx: u64, version: u64, core_count: u64, shared_buffer_addr:
     handle_incoming_calls(regs)
 }
 
-fn warmboot_main(pe_idx: u64, activation_token: u64) -> ! {
-    let log_sink = PlatformImpl::make_log_sink();
-    logger::init(log_sink).unwrap();
+fn secondary_main(pe_idx: u64, activation_token: u64) -> ! {
+    set_exception_vector();
 
     info!(
         "Fake RMM warmboot at EL {} with args {:#x}, {:#x}",
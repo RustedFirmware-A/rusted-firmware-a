RustedFirmware-A/rusted-firmware-a

commit 20e36caf5541dc93e04e4d20720b274fe393d3ec

feat(rme): add payload for R-EL2 & build instructions
Introduces a new STF image which runs in the Realm World, instantly
returning to the Root World with the SMC indicating a successful R-EL2
boot.

This patch also updates the FVP arguments in `build-and-run.sh` when
RME is enabled, based on the shrinkwrap configuration for running 4
worlds.

Change-Id: Ifc97c51d722d991bfaa1da1708aae8cceb9b1c8c
Signed-off-by: Ludovic Mermod <ludovic.mermod@arm.com>

secure_test_framework/src/stf_rmm.rs
@@ -1,0 +1,73 @@
+// Copyright The Rusted Firmware-A Contributors.
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+//! Fake RMM component of RF-A Secure Test Framework.
+
+#![no_main]
+#![no_std]
+#![allow(dead_code)]
+
+extern crate alloc;
+
+mod exceptions;
+mod ffa;
+mod framework;
+mod gicv3;
+mod heap;
+mod logger;
+mod pagetable;
+mod platform;
+mod tests;
+mod util;
+
+use core::panic::PanicInfo;
+
+use aarch64_rt::{enable_mmu, entry};
+use log::{error, info};
+use smccc::{psci, smc64};
+
+use crate::{
+    platform::{Platform, PlatformImpl, RMM_IDMAP},
+    util::current_el,
+};
+
+const RMM_BOOT_COMPLETE: u32 = 0xC400_01CF;
+
+enable_mmu!(RMM_IDMAP);
+
+entry!(realm_main, 4);
+fn realm_main(x0: u64, x1: u64, x2: u64, x3: u64) -> ! {
+    let log_sink = PlatformImpl::make_log_sink();
+    logger::init(log_sink).unwrap();
+
+    info!(
+        "Fake RMM starting at EL {} with args {:#x}, {:#x}, {:#x}, {:#x}",
+        current_el(),
+        x0,
+        x1,
+        x2,
+        x3,
+    );
+
+    let args = [0; 17];
+    smc64(RMM_BOOT_COMPLETE, args);
+
+    // The `smc` instruction in the previous asm section never returns.
+    unreachable!()
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    error!("{info}");
+    loop {}
+}
+
+fn call_test_helper(_: usize, _: [u64; 3]) -> Result<[u64; 4], ()> {
+    panic!("call_test_helper shouldn't be called from realm world tests");
+}
+
+/// Not supported in RMM.
+pub fn start_secondary(psci_mpidr: u64, _entry: fn(u64) -> !, arg: u64) -> Result<(), psci::Error> {
+    panic!("start_secondary({psci_mpidr:#}, .., {arg}) called in RMM");
+}

src/gicv3.rs
@@ -351,7 +351,10 @@ pub fn set_routing_model(scr_el3: &mut ScrEl3, world: World) {
             *scr_el3 -= ScrEl3::FIQ;
         }
         #[cfg(feature = "rme")]
-        World::Realm => todo!("Routing model for Realms not configured."),
+        World::Realm => {
+            *scr_el3 -= ScrEl3::FIQ;
+            *scr_el3 -= ScrEl3::IRQ;
+        }
     }
 }
 

Makefile
@@ -12,6 +12,7 @@ endif
 BL31_BIN := $(OUT)/bl31.bin
 BL32 := $(OUT)/bl32.bin
 BL33 := $(OUT)/bl33.bin
+STF_RMM := $(OUT)/stf_rmm.bin
 FIP := $(OUT)/fip.bin
 BL31_ELF := $(OUT)/bl31.elf
 
@@ -33,12 +34,16 @@ ifndef PLAT
   endif
 endif
 
-STF_CARGO_FLAGS := --release
+STF_CARGO_FLAGS = --release  --features "$(STF_FEATURES)"
 STF_IMAGES_FLAGS = $(patsubst target/%.bin, "--bin" "%", $(STF_IMAGES))
 RFA_CARGO_FLAGS := --no-default-features
 
 # List of test images that can be built for that platform.
 STF_IMAGES := $(BL32) $(BL33)
+ifeq (${RME}, 1)
+	STF_FEATURES += rme
+	STF_IMAGES += $(STF_RMM)
+endif
 
 # Make a release build by default.
 DEBUG ?= 0
@@ -116,6 +121,8 @@ $(BL32): build-stf
 	$(OBJCOPY) $(OUT)/$(TARGET)/release/bl32 -O binary $@
 $(BL33): build-stf
 	$(OBJCOPY) $(OUT)/$(TARGET)/release/bl33 -O binary $@
+$(STF_RMM): build-stf
+	$(OBJCOPY) $(OUT)/$(TARGET)/release/stf_rmm -O binary $@
 
 clippy-test:
 	RUSTFLAGS="-D warnings" $(CARGO) clippy --tests --features "$(FEATURES)"
@@ -132,7 +139,7 @@ clippy:
 		$(STF_CARGO_FLAGS) \
 		$(STF_IMAGES_FLAGS)
 
-images: $(BL32) $(BL33) build
+images: $(STF_IMAGES) build
 
 clean:
 	$(CARGO) clean

build-and-run.sh
@@ -49,17 +49,19 @@ BL32="${BL32:-${OUT}/bl32.bin}"
 BL33="${BL33:-${OUT}/bl33.bin}"
 FIP=${TFA}/build/${PLAT}/${BUILDTYPE}/fip.bin
 
+STF_RMM=$OUT/stf_rmm.bin
+
 CURRDIR=$(readlink -f "$(dirname "$0")")
 pushd $CURRDIR
 
 QEMU=${QEMU:-qemu-system-aarch64}
 GDB_PORT=${GDB_PORT:-1234}
 QEMU_FLAGS=${QEMU_FLAGS:--machine virt,gic-version=3,secure=on,virtualization=on -cpu max -m 1204M \
-	-chardev stdio,signal=off,mux=on,id=char0 -monitor chardev:char0 \
-	-serial chardev:char0 -serial chardev:char0 -semihosting-config enable=on,target=native \
-	-gdb tcp:localhost:${GDB_PORT} \
-	-display none -bios ${BL1} \
-	-smp 4}
+    -chardev stdio,signal=off,mux=on,id=char0 -monitor chardev:char0 \
+    -serial chardev:char0 -serial chardev:char0 -semihosting-config enable=on,target=native \
+    -gdb tcp:localhost:${GDB_PORT} \
+    -display none -bios ${BL1} \
+    -smp 4}
 
 case "$PLAT" in
   qemu)
@@ -141,58 +143,52 @@ case "$PLAT" in
     # (because a debug build of RF-A is too big to fit in 256 kB). The `FVP_TRUSTED_SRAM_SIZE=512` TF-A
     # build flag is required to stop TF-A from complaining that RF-A does not fit.
     if [[ "${RME:-}" == 1 ]]; then
+        RMM=${RMM:-"$STF_RMM"}
         make PLAT=fvp FEATURES=sel2,rme ${DEBUG} CARGO="${CARGO}" PAUTH_EL3=${PAUTH_EL3} PAUTH_LR_EL3=${PAUTH_LR_EL3} BTI_EL3=${BTI_EL3} all
-        make -C $TFA PLAT=fvp ${DEBUG} FVP_TRUSTED_SRAM_SIZE=512 ENABLE_RME=1 \
+        make -C $TFA PLAT=fvp ${DEBUG} FVP_TRUSTED_SRAM_SIZE=512 ENABLE_RME=1 RMM="$RMM" SPD=spmd SPMD_SPM_AT_SEL2=0 CTX_INCLUDE_AARCH32_REGS=0 \
             BL31=${OUT}/bl31.bin \
             BL32=${BL32} \
             BL33=${BL33} \
             all fip
         FVP_Base_RevC-2xAEMvA \
-            -C bp.secure_memory=0 \
-            -Q 1000 \
-            -C bp.refcounter.non_arch_start_at_default=1 \
+            -C bp.dram_size=4 \
             -C bp.has_rme=1 \
-            -C bp.dram_metadata.is_enabled=1 \
-            -C bp.ls64_testing_fifo.op_type=0 \
-            -C cluster0.restriction_on_speculative_execution=2 \
-            -C cluster0.restriction_on_speculative_execution_aarch32=2 \
-            -C cluster0.stage12_tlb_size=1024 \
-            -C cluster0.check_memory_attributes=0 \
+            -C bp.secure_memory=0 \
+            -C cluster0.PA_SIZE=48 \
+            -C cluster0.gicv3.cpuintf-mmap-access-level=2 \
+            -C cluster0.gicv3.without-DS-support=1 \
+            -C cluster0.gicv4.mask-virtual-interrupt=1 \
+            -C cluster0.rme_support_level=2 \
+            -C cluster1.PA_SIZE=48 \
+            -C cluster1.gicv3.cpuintf-mmap-access-level=2 \
+            -C cluster1.gicv3.without-DS-support=1 \
+            -C cluster1.gicv4.mask-virtual-interrupt=1 \
+            -C cluster1.rme_support_level=2 \
+            -C cluster0.has_rndr=1 \
+            -C cluster1.has_rndr=1 \
+            -C cluster0.output_attributes="ExtendedID[62:55]=MPAM_PMG,ExtendedID[54:39]=MPAM_PARTID,ExtendedID[38]=MPAM_SP[0],ExtendedID[37]=MPAM_SP[1]" \
+            -C cluster1.output_attributes="ExtendedID[62:55]=MPAM_PMG,ExtendedID[54:39]=MPAM_PARTID,ExtendedID[38]=MPAM_SP[0],ExtendedID[37]=MPAM_SP[1]" \
+            -C cluster0.pmb_idr_external_abort=1 \
+            -C cluster1.pmb_idr_external_abort=1 \
+            -C cluster0.has_arm_v8-8=1 \
+            -C cluster1.has_arm_v8-8=1 \
+            -C cluster0.has_arm_v9-2=1 \
+            -C cluster1.has_arm_v9-2=1 \
+            -C cluster0.max_32bit_el=0 \
+            -C cluster1.max_32bit_el=0 \
+            -C cluster0.has_tcr2=2 \
+            -C cluster1.has_tcr2=2 \
             -C pci.pci_smmuv3.mmu.SMMU_AIDR=2 \
-            -C pci.pci_smmuv3.mmu.SMMU_IDR1=0x00600002 \
+            -C pci.pci_smmuv3.mmu.SMMU_IDR0=0x080FF6BF \
+            -C pci.pci_smmuv3.mmu.SMMU_IDR1=0x0CE73D20 \
             -C pci.pci_smmuv3.mmu.SMMU_IDR3=0x1714 \
+            -C pci.pci_smmuv3.mmu.SMMU_IDR5=0xFFFF0475 \
             -C pci.pci_smmuv3.mmu.SMMU_S_IDR1=0xA0000002 \
             -C pci.pci_smmuv3.mmu.SMMU_S_IDR2=0 \
             -C pci.pci_smmuv3.mmu.SMMU_S_IDR3=0 \
-            -C pci.pci_smmuv3.mmu.SMMU_IDR0=0x4046123b \
-            -C pci.pci_smmuv3.mmu.SMMU_IDR5=0xFFFF0475 \
-            -C pci.pci_smmuv3.mmu.SMMU_ROOT_IDR0=3 \
+            -C pci.pci_smmuv3.mmu.SMMU_ROOT_IDR0=0x3 \
             -C pci.pci_smmuv3.mmu.SMMU_ROOT_IIDR=0x43B \
             -C pci.pci_smmuv3.mmu.root_register_page_offset=0x20000 \
-            -C cluster0.has_arm_v9-2=1 \
-            -C cluster1.has_arm_v9-2=1 \
-            -C cluster0.rme_support_level=2 \
-            -C cluster0.gicv3.cpuintf-mmap-access-level=2 \
-            -C cluster0.gicv4.mask-virtual-interrupt=1 \
-            -C cluster0.gicv3.without-DS-support=1 \
-            -C cluster0.max_32bit_el=-1 \
-            -C cluster0.PA_SIZE=48 \
-            -C cluster0.output_attributes=ExtendedID[62:55]=MPAM_PMG,ExtendedID[54:39]=MPAM_PARTID,ExtendedID[38:37]=MPAM_SP \
-            -C cluster0.has_rndr=1 \
-            -C cluster0.arm_v8_7_accelerator_support_level="" \
-            -C cluster1.restriction_on_speculative_execution=2 \
-            -C cluster1.restriction_on_speculative_execution_aarch32=2 \
-            -C cluster1.stage12_tlb_size=1024 \
-            -C cluster1.check_memory_attributes=0 \
-            -C cluster1.rme_support_level=2 \
-            -C cluster1.gicv3.cpuintf-mmap-access-level=2 \
-            -C cluster1.gicv4.mask-virtual-interrupt=1 \
-            -C cluster1.gicv3.without-DS-support=1 \
-            -C cluster1.max_32bit_el=-1 \
-            -C cluster1.PA_SIZE=48 \
-            -C cluster1.output_attributes=ExtendedID[62:55]=MPAM_PMG,ExtendedID[54:39]=MPAM_PARTID,ExtendedID[38:37]=MPAM_SP \
-            -C cluster1.has_rndr=1 \
-            -C cluster1.arm_v8_7_accelerator_support_level="" \
             ${FVP_COMMON_ARGS}
 
     else

secure_test_framework/Cargo.toml
@@ -42,6 +42,7 @@ thiserror = { version = "2.0.17", default-features = false }
 
 [features]
 pauth = []
+rme = []
 
 [[bin]]
 name = "bl32"
@@ -50,6 +51,10 @@ path = "src/bl32.rs"
 [[bin]]
 name = "bl33"
 path = "src/bl33.rs"
+
+[[bin]]
+name = "stf_rmm"
+path = "src/stf_rmm.rs"
 
 [lints.clippy]
 missing_safety_doc = "deny"

secure_test_framework/build.rs
@@ -21,4 +21,10 @@ fn main() {
     println!("cargo:rustc-link-arg-bin=bl33=-T{crate_dir}/{platform}_bl33.ld");
     println!("cargo:rerun-if-changed={crate_dir}/{platform}_bl32.ld");
     println!("cargo:rerun-if-changed={crate_dir}/{platform}_bl33.ld");
+
+    #[cfg(feature = "rme")]
+    {
+        println!("cargo:rustc-link-arg-bin=stf_rmm=-T{crate_dir}/{platform}_stf_rmm.ld");
+        println!("cargo:rerun-if-changed={crate_dir}/{platform}_stf_rmm.ld");
+    }
 }

secure_test_framework/fvp_stf_rmm.ld
@@ -1,0 +1,10 @@
+/*
+ * Copyright The Rusted Firmware-A Contributors.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+MEMORY
+{
+	image : ORIGIN = 0xfdc00000, LENGTH = 0x2000000
+}

secure_test_framework/src/platform.rs
@@ -12,7 +12,7 @@ use arm_sysregs::{MpidrEl1, read_mpidr_el1};
 use core::fmt::Write;
 #[cfg(platform = "fvp")]
 #[allow(unused)]
-pub use fvp::{BL32_IDMAP, BL33_IDMAP};
+pub use fvp::{BL32_IDMAP, BL33_IDMAP, RMM_IDMAP};
 use percore::Cores;
 #[cfg(platform = "qemu")]
 #[allow(unused)]

commit cbf862076b8e7f3a9a6b3c283740be3a26695de2

fix(rme): address coding convention mismatch in stf_rmm
Fixes several divergences from RF-A's coding convention in the STF RMM
image.

Fixes: 20e36ca ("feat(rme): add payload for R-EL2 & build instructions")
Change-Id: I9705fd29c70c09a2d90c62aaccd23c4da09ef215
Signed-off-by: Ludovic Mermod <ludovic.mermod@arm.com>

secure_test_framework/src/ffa.rs
@@ -14,6 +14,7 @@ use smccc::{arch, error::positive_or_error_32, smc64};
 /// The FF-A version which we implement here.
 const FFA_VERSION: Version = Version(1, 2);
 
+#[allow(unused)]
 pub fn version(input_version: Version) -> Result<Version, arch::Error> {
     assert_eq!(input_version.0 & 0x8000, 0);
     let output_version = positive_or_error_32::<arch::Error>(

secure_test_framework/src/gicv3.rs
@@ -96,6 +96,7 @@ pub fn handle_group1_interrupt() {
 }
 
 /// Enables IRQ handling for the current EL.
+#[allow(unused)]
 pub fn init(secure: bool) {
     // Safety: This is the only place where PlatformImpl::create_gic is called.
     GIC.call_once(|| SpinMutex::new(unsafe { PlatformImpl::create_gic() }));

secure_test_framework/src/heap.rs
@@ -18,6 +18,7 @@ static HEAP: SpinMutex<[u8; HEAP_SIZE]> = SpinMutex::new([0; HEAP_SIZE]);
 static HEAP_ALLOCATOR: LockedHeap<32> = LockedHeap::new();
 
 /// Initialises the heap allocator.
+#[allow(unused)]
 pub fn init() {
     // Give the allocator some memory to allocate.
     add_to_heap(

secure_test_framework/src/stf_rmm.rs
@@ -6,7 +6,6 @@
 
 #![no_main]
 #![no_std]
-#![allow(dead_code)]
 
 extern crate alloc;
 
@@ -24,6 +23,7 @@ mod util;
 
 use core::{arch::naked_asm, panic::PanicInfo, ptr::read};
 
+use aarch64_paging::paging::PAGE_SIZE;
 use aarch64_rt::{enable_mmu, entry, set_exception_vector};
 use log::{error, info};
 use smccc::{psci, smc64};
@@ -72,17 +72,30 @@ impl From<RmmBootManifestVersion> for u32 {
         (value.major as u32) << 16 | value.minor as u32
     }
 }
+impl From<RmmBootManifestVersion> for u64 {
+    fn from(value: RmmBootManifestVersion) -> Self {
+        u32::from(value) as u64
+    }
+}
 
+/// Value returned by RMM after a cold/warmboot.
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
-#[allow(missing_docs)]
 pub enum RmmBootReturn {
+    /// Boot successful.
     Success = 0,
+    /// Unknown error.
     Unknown = -1,
+    /// Boot Interface version reported by EL3 is not supported by RMM.
     VersionNotValid = -2,
+    /// Number of CPUs reported by EL3 larger than maximum supported by RMM.
     CpusOutOfRange = -3,
+    /// Current CPU Id is higher or equal than the number of CPUs supported by RMM.
     CpuIdOutOfRange = -4,
+    /// Invalid pointer to shared memory area.
     InvalidSharedBuffer = -5,
+    /// Version reported by the Boot Manifest not supported by RMM.
     ManifestVersionNotSupported = -6,
+    /// Error parsing core Boot Manifest.
     ManifestDataError = -7,
 }
 
@@ -92,6 +105,7 @@ enum RmiStatusCode {
     Success = 0,
     ErrorInput = 1,
     ErrorRealm = 2,
+    #[allow(unused)]
     ErrorRec = 3,
     ErrorRtt = 4,
 }
@@ -166,7 +180,7 @@ fn handle_incoming_calls(mut regs: [u64; 18]) -> ! {
 /// Handles a single RMI call from NS world.
 fn handle_rmi_call(regs: &[u64]) -> [u64; 17] {
     match regs[0] {
-        RMM_RMI_REQ_VERSION => rmi_version(regs),
+        RMM_RMI_REQ_VERSION => rmi_version(&regs[1..]),
         _ => {
             let mut ret = [0; 17];
             ret[0] = u64::MAX;
@@ -178,7 +192,7 @@ fn handle_rmi_call(regs: &[u64]) -> [u64; 17] {
 fn rmi_version(args: &[u64]) -> [u64; 17] {
     let mut ret = [0; 17];
 
-    let Ok(requested) = RmmBootManifestVersion::try_from(args[1] as u32) else {
+    let Ok(requested) = RmmBootManifestVersion::try_from(args[0] as u32) else {
         ret[0] = RmiCommandReturnCode::new(RmiStatusCode::ErrorInput, None).into();
         return ret;
     };
@@ -188,15 +202,13 @@ fn rmi_version(args: &[u64]) -> [u64; 17] {
         requested.major, requested.minor
     );
 
-    let supported = u32::from(SUPPORTED_RMM_VERSION) as u64;
-
-    ret[0] = if args[1] == supported {
+    ret[0] = if requested == SUPPORTED_RMM_VERSION {
         RmiCommandReturnCode::new(RmiStatusCode::Success, None).into()
     } else {
         RmiCommandReturnCode::new(RmiStatusCode::ErrorInput, None).into()
     };
-    ret[1] = supported;
-    ret[2] = supported;
+    ret[1] = SUPPORTED_RMM_VERSION.into();
+    ret[2] = SUPPORTED_RMM_VERSION.into();
 
     ret
 }
@@ -213,21 +225,25 @@ fn validate_coldboot_args(pe_idx: u64, version: u64, core_count: u64, shared_buf
 
     if version.major != SUPPORTED_RMM_VERSION.major {
         complete_boot(RmmBootReturn::VersionNotValid, &[]);
+        unreachable!()
     }
 
     if shared_buffer_addr == 0 {
         complete_boot(RmmBootReturn::InvalidSharedBuffer, &[]);
+        unreachable!()
     }
 
     // Shared buffer must be paged-aligned.
-    if !shared_buffer_addr.is_multiple_of(0x1000) {
+    if !shared_buffer_addr.is_multiple_of(PAGE_SIZE as u64) {
         complete_boot(RmmBootReturn::InvalidSharedBuffer, &[]);
+        unreachable!()
     }
 
     // TODO: map the shared buffer into memory.
 
     if core_count > PlatformImpl::CORE_COUNT as u64 {
         complete_boot(RmmBootReturn::CpusOutOfRange, &[]);
+        unreachable!()
     }
 }
 
@@ -296,6 +312,7 @@ fn rmm_main(pe_idx: u64, version: u64, core_count: u64, shared_buffer_addr: u64)
     if manifest_version.major != SUPPORTED_RMM_MANIFEST_VERSION.major {
         error!("Unsupported manifest version: 0x{manifest_version:x?}");
         complete_boot(RmmBootReturn::ManifestVersionNotSupported, &[]);
+        unreachable!()
     }
 
     let regs = complete_boot(RmmBootReturn::Success, &[0xdead_beef]);

secure_test_framework/src/util.rs
@@ -12,6 +12,7 @@ use log::error;
 pub const NORMAL_WORLD_ID: u16 = 0;
 
 /// The partition ID of the secure world component.
+#[allow(unused)]
 pub const SECURE_WORLD_ID: u16 = 0x8001;
 
 /// Default ID for the SPMC

secure_test_framework/src/tests/rmi.rs
@@ -6,8 +6,14 @@ use smccc::smc64;
 
 use crate::{
     expect,
-    framework::{TestResult, expect::expect_eq, normal_world_test},
+    framework::{
+        TestResult,
+        expect::{expect_eq, fail},
+        normal_world_test,
+    },
 };
+
+const RMM_RMI_REQ_VERSION: u32 = 0xC400_0150;
 
 normal_world_test!(test_rmm_version);
 fn test_rmm_version() -> TestResult {
@@ -16,7 +22,7 @@ fn test_rmm_version() -> TestResult {
     let mut args = [0; 17];
     args[0] = REQUESTED_VERSION;
 
-    let ret = smc64(0xC400_0150u32, args);
+    let ret = smc64(RMM_RMI_REQ_VERSION, args);
 
     // Call not supported, i.e. there is no RMMD.
     if ret[0] == u64::MAX {
@@ -36,7 +42,7 @@ fn test_rmm_version() -> TestResult {
             expect_eq!(lower, REQUESTED_VERSION);
         }
         1 => expect!(lower != REQUESTED_VERSION),
-        _ => expect!(false),
+        v => fail!("Invalid return code from RMM_RMI_REQ_VERSION: {v}"),
     }
 
     Ok(())
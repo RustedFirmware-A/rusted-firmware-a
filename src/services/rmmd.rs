RustedFirmware-A/rusted-firmware-a

commit fb2ec360d7a040d021c89880bcb1ab9b5e06173d

feat(rme): allocate boot manifest for R-EL2 payload
When RME is enabled, EL3 will allocate shared buffer before booting to
communicate with the R-EL2 payload and initialize the RMM Boot Manifest
at its start.

This change also adds data structure definitions to manipulate the Boot
Manifest from within EL3. This could later be moved to a dedicated crate
if pertinent.

Change-Id: Ibd3cf332662d0d58f4d7d7accf7de114887dbd91
Signed-off-by: Ludovic Mermod <ludovic.mermod@arm.com>

src/services/rmmd.rs
@@ -2,8 +2,7 @@
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
-use core::cell::RefCell;
-
+use core::{cell::RefCell, slice::from_raw_parts_mut};
 use percore::{Cores, ExceptionLock, PerCore};
 use spin::Once;
 
@@ -16,6 +15,40 @@ use crate::{
 };
 
 const RMM_BOOT_VERSION: u64 = 0x5;
+/// Size in bytes of the EL3 - RMM shared area.
+pub const RMM_SHARED_BUFFER_SIZE: usize = 0x1000;
+
+pub mod manifest;
+/// Returns a mutable reference to the shared buffer used for communication between R-EL2 and EL3.
+///
+/// # Safety
+///
+/// Calling this function is always safe, but using its return value is safe if all the conditions
+/// below are met:
+///
+/// - It can only be called after the shared buffer is mapped into the page table.
+/// - After calling `get_shared_buffer`, the return reference must be dropped before any other call
+///   to it is made.
+/// - The reference must be dropped before switching to Realm World.
+unsafe fn get_shared_buffer() -> &'static mut [u8; RMM_SHARED_BUFFER_SIZE] {
+    // Safety: (relative to [`slice::from_raw_parts_mut`][https://doc.rust-lang.org/stable/core/slice/fn.from_raw_parts_mut.html])
+    // - The first condition of `get_shared_buffer()` ensures that the location is valid, and as it
+    //   occupies exactly one page, it will always be aligned.
+    // - `u8` is properly initialized regardless of the initial value.
+    // - The second condition ensures that the buffer is never accessed through multiple reference
+    //   within EL3. As it can only be accessed by EL3 and Realm World, it follows from the third
+    //   condition that no other pointers can be used to access the buffer while a reference exists.
+    // - Follows from the soundness of the layout defined in `layout.rs`.
+    unsafe {
+        from_raw_parts_mut(
+            PlatformImpl::RMM_SHARED_BUFFER_START as *mut u8,
+            RMM_SHARED_BUFFER_SIZE,
+        )
+        .try_into()
+        .unwrap()
+    }
+}
+
 const RMM_BOOT_COMPLETE: u32 = 0xC400_01CF;
 
 #[derive(Debug)]
@@ -68,6 +101,15 @@ impl Rmmd {
             [const { ExceptionLock::new(RefCell::new(RmmdLocal::new())) };
                 PlatformImpl::CORE_COUNT],
         );
+
+        // Safety:
+        // - This function is called after initializing the MMU and pagetable.
+        // - This function never calls again `get_shared_buffer()`, thus the reference will be dropped
+        //   upon return, before another call is made.
+        // - Similarly to the above, this function does not switch to the Realm World.
+        let buf = unsafe { get_shared_buffer() };
+        PlatformImpl::rme_prepare_manifest(buf);
+        info!("RMM Boot Manifest ready");
 
         Self { core_local }
     }

Cargo.toml
@@ -41,6 +41,7 @@ arm-sysregs = { version = "0.2.1", features = ["el3"] }
 arrayvec = { version = "0.7.6", default-features = false }
 bitflags = "2.10.0"
 log = "0.4.29"
+paste = "1.0.14"
 percore = { version = "0.2.1", default-features = false, features = [
   "zerocopy",
 ] }

secure_test_framework/src/platform/fvp.rs
@@ -177,6 +177,7 @@ BL33_IDMAP:
 // 0x1C09_0000 PL011
 // 0x2f00_0000 GIC
 // 0xfdc0_0000 image
+// 0xffb0_0000 shared buffer TODO: this should be mapped at runtime based on the coldboot parameters
 global_asm!(
     "
 .section \".rodata.RMM_IDMAP\", \"a\", %progbits
@@ -200,7 +201,9 @@ RMM_IDMAP:
 1:
     .fill 494, 8, 0x0
     .quad {MEMORY_ATTRIBUTES} | 0xfdc00000
-    .fill 17, 8, 0x0
+    .fill 14, 8, 0x0
+    .quad {MEMORY_ATTRIBUTES} | 0xffb00000
+    .fill 2, 8, 0x0
 ",
     DEVICE_ATTRIBUTES = const DEVICE_ATTRIBUTES.bits(),
     MEMORY_ATTRIBUTES = const MEMORY_ATTRIBUTES.bits(),

secure_test_framework/src/stf_rmm.rs
@@ -23,6 +23,7 @@ mod util;
 
 use core::{
     panic::PanicInfo,
+    slice::from_raw_parts_mut,
     sync::atomic::{AtomicBool, Ordering},
 };
 
@@ -99,7 +100,7 @@ fn complete_boot(ret: RmmBootReturn, extra_args: &[u64]) -> ! {
     todo!()
 }
 
-fn validate_coldboot_args(pe_idx: u64, version: u64, core_count: u64, _shared_buffer_addr: u64) {
+fn validate_coldboot_args(pe_idx: u64, version: u64, core_count: u64, shared_buffer_addr: u64) {
     if pe_idx >= core_count {
         complete_boot(RmmBootReturn::CpuIdOutOfRange, &[]);
     }
@@ -112,7 +113,16 @@ fn validate_coldboot_args(pe_idx: u64, version: u64, core_count: u64, _shared_bu
         complete_boot(RmmBootReturn::VersionNotValid, &[])
     }
 
-    // TODO: check shared buffer
+    if shared_buffer_addr == 0 {
+        complete_boot(RmmBootReturn::InvalidSharedBuffer, &[]);
+    }
+
+    // Shared buffer must be paged-aligned.
+    if !shared_buffer_addr.is_multiple_of(0x1000) {
+        complete_boot(RmmBootReturn::InvalidSharedBuffer, &[]);
+    }
+
+    // TODO: map the shared buffer into memory.
 
     if core_count > PlatformImpl::CORE_COUNT as u64 {
         complete_boot(RmmBootReturn::CpusOutOfRange, &[]);
@@ -150,6 +160,25 @@ fn coldboot_main(pe_idx: u64, version: u64, core_count: u64, shared_buffer_addr:
         core_count,
         shared_buffer_addr,
     );
+
+    // Safety: the specification states that the `x3` register of the RMM is a pointer to a 4KB
+    // page mapped into the Realm World.
+    let shared_buf =
+        unsafe { from_raw_parts_mut(shared_buffer_addr as *mut u32, 0x1000 / size_of::<u32>()) };
+
+    let Ok(manifest_version) = RmmBootManifestVersion::try_from(shared_buf[0]) else {
+        complete_boot(RmmBootReturn::VersionNotValid, &[]);
+    };
+
+    info!(
+        "Received manifest with version v{}.{}",
+        manifest_version.major, manifest_version.minor
+    );
+
+    if manifest_version.major != SUPPORTED_RMM_MANIFEST_VERSION.major {
+        error!("Unsupported manifest version: 0x{manifest_version:x?}");
+        complete_boot(RmmBootReturn::ManifestVersionNotSupported, &[])
+    }
 
     complete_boot(RmmBootReturn::Success, &[0xdead_beef])
 }

src/pagetable.rs
@@ -4,6 +4,8 @@
 
 pub mod early_pagetable;
 
+#[cfg(feature = "rme")]
+use crate::services::rmmd::RMM_SHARED_BUFFER_SIZE;
 use crate::{
     aarch64::{dsb_sy, isb, tlbi_alle3},
     layout::{
@@ -83,15 +85,15 @@ const NSE: Attributes = Attributes::NON_GLOBAL;
 /// Attributes used for all mappings.
 ///
 /// We always set the access flag, as we don't manage access flag faults.
-const BASE: Attributes = if cfg!(feature = "rme") {
-    EL3_RES1
-        .union(Attributes::ACCESSED)
-        .union(Attributes::VALID)
-        .union(NSE)
+const BASE: Attributes = EL3_RES1
+    .union(Attributes::ACCESSED)
+    .union(Attributes::VALID);
+
+/// Attributes used for all mappings in EL3.
+const BASE_EL3: Attributes = if cfg!(feature = "rme") {
+    BASE.union(NSE)
 } else {
-    EL3_RES1
-        .union(Attributes::ACCESSED)
-        .union(Attributes::VALID)
+    BASE
 };
 
 /// Attributes used for device mappings.
@@ -104,33 +106,70 @@ const BASE: Attributes = if cfg!(feature = "rme") {
 /// region has an effective Shareability attribute of Outer Shareable."
 /// Arm ARM K.a D8.4.1.2.3 bit 54 UXN/PXN/XN is the XN field at EL3:
 /// "If the Effective value of XN is 1, then PrivExecute is removed."
-pub const MT_DEVICE: Attributes = DEVICE.union(BASE).union(Attributes::UXN);
+pub const MT_DEVICE: Attributes = DEVICE.union(BASE_EL3).union(Attributes::UXN);
 
-/// Attributes used for non-cacheable memory mappings.
-#[allow(unused)]
-pub const MT_NON_CACHEABLE: Attributes = NON_CACHEABLE.union(BASE);
+/// Generates memory attributes for all available worlds.
+///
+/// The attributes created are:
+/// - `MT_NON_CACHEABLE`
+/// - `MT_MEMORY`
+/// - `MT_RO_DATA`
+/// - `MT_RW_DATA`
+/// - `MT_CODE`
+macro_rules! make_memory_attributes {
+    ($name:ident, $base:expr) => {
+        paste::paste! {
+            #[doc = "Attributes used for non-cacheable memory mappings in "]
+            #[doc = stringify!($name)]
+            #[doc = " world."]
+            #[allow(unused)]
+            pub const [<MT_NON_CACHEABLE_ $name>]: Attributes = NON_CACHEABLE.union($base);
 
-/// Attributes used for regular memory mappings.
-pub const MT_MEMORY: Attributes = NORMAL_MEMORY.union(BASE).union(Attributes::INNER_SHAREABLE);
+            #[doc = "Attributes used for all memory mappings in "]
+            #[doc = stringify!($name)]
+            #[doc = " world."]
+            #[allow(unused)]
+            pub const [<MT_MEMORY_ $name>]: Attributes = NORMAL_MEMORY
+                .union($base)
+                .union(Attributes::INNER_SHAREABLE);
 
-/// Attributes used for code (i.e. text) mappings.
-pub const MT_CODE: Attributes = {
-    let attrs = MT_MEMORY.union(Attributes::READ_ONLY);
-    if cfg!(bti) {
-        attrs.union(Attributes::GP)
-    } else {
-        attrs
-    }
-};
 
-/// Attributes used for read-only data mappings.
-pub const MT_RO_DATA: Attributes = MT_MEMORY
-    .union(Attributes::READ_ONLY)
-    .union(Attributes::UXN);
+            #[doc = "Attributes used for read-only data mappings in "]
+            #[doc = stringify!($name)]
+            #[doc = " world."]
+            #[allow(unused)]
+            pub const [<MT_RO_DATA_ $name>]: Attributes = [<MT_MEMORY_ $name>]
+                .union(Attributes::READ_ONLY)
+                .union(Attributes::UXN);
 
-/// Attributes used for read-write data mappings.
-#[allow(unused)]
-pub const MT_RW_DATA: Attributes = MT_MEMORY.union(Attributes::UXN);
+            #[doc = "Attributes used for read-write data mappings in "]
+            #[doc = stringify!($name)]
+            #[doc = " world."]
+            #[allow(unused)]
+            pub const [<MT_RW_DATA_ $name>]: Attributes = [<MT_MEMORY_ $name>].union(Attributes::UXN);
+
+
+            #[doc = "Attributes used for code (i.e. text) mappings in "]
+            #[doc = stringify!($name)]
+            #[doc = " world."]
+            #[allow(unused)]
+            pub const [<MT_CODE_ $name>]: Attributes = {
+                let attrs = [<MT_MEMORY_ $name>].union(Attributes::READ_ONLY);
+                if cfg!(bti) {
+                    attrs.union(Attributes::GP)
+                } else {
+                    attrs
+                }
+            };
+        }
+    };
+}
+
+make_memory_attributes!(EL3, BASE_EL3);
+make_memory_attributes!(SECURE, BASE);
+make_memory_attributes!(NS, BASE.union(Attributes::NS));
+#[cfg(feature = "rme")]
+make_memory_attributes!(REALM, BASE.union(Attributes::NS).union(NSE));
 
 static PAGE_HEAP: SpinMutex<[PageTable; PlatformImpl::PAGE_HEAP_PAGE_COUNT]> =
     SpinMutex::new([PageTable::EMPTY; PlatformImpl::PAGE_HEAP_PAGE_COUNT]);
@@ -268,18 +307,30 @@ fn init_page_table(pages: &'static mut [PageTable]) -> IdMap {
     unsafe {
         // Corresponds to `bl_regions` in C TF-A, `plat/arm/common/arm_bl31_setup.c`.
         // BL31_TOTAL
-        idmap.map_region(&MemoryRegion::new(bl31_start(), bl31_end()), MT_MEMORY);
+        idmap.map_region(&MemoryRegion::new(bl31_start(), bl31_end()), MT_MEMORY_EL3);
         // BL31_RO
-        idmap.map_region(&MemoryRegion::new(bl_code_base(), bl_code_end()), MT_CODE);
+        idmap.map_region(
+            &MemoryRegion::new(bl_code_base(), bl_code_end()),
+            MT_CODE_EL3,
+        );
         idmap.map_region(
             &MemoryRegion::new(bl_ro_data_base(), bl_ro_data_end()),
-            MT_RO_DATA,
+            MT_RO_DATA_EL3,
         );
         let bss2_start = bss2_start();
         let bss2_end = bss2_end();
         if bss2_start != bss2_end {
-            idmap.map_region(&MemoryRegion::new(bss2_start, bss2_end), MT_RW_DATA);
+            idmap.map_region(&MemoryRegion::new(bss2_start, bss2_end), MT_RW_DATA_EL3);
         }
+
+        #[cfg(feature = "rme")]
+        idmap.map_region(
+            &MemoryRegion::new(
+                PlatformImpl::RMM_SHARED_BUFFER_START,
+                PlatformImpl::RMM_SHARED_BUFFER_START + RMM_SHARED_BUFFER_SIZE,
+            ),
+            MT_RW_DATA_REALM,
+        );
     }
 
     // Corresponds to `plat_regions` in C TF-A.

src/platform.rs
@@ -39,6 +39,8 @@ select_platform!(platform = "fvp", fvp::Fvp);
 select_platform!(platform = "qemu", qemu::Qemu);
 select_platform!(test, test::TestPlatform);
 
+#[cfg(feature = "rme")]
+use crate::services::rmmd::RMM_SHARED_BUFFER_SIZE;
 use crate::{
     context::EntryPointInfo,
     cpu_extensions::CpuExtension,
@@ -122,6 +124,11 @@ pub unsafe trait Platform {
     /// Base address for the EL3 - RMM shared area.
     #[cfg(feature = "rme")]
     const RMM_SHARED_BUFFER_START: usize;
+    /// Platform dependent part of the RMM Boot Manifest. Entries within the range `0..RMM_<NAME>`
+    /// (see above) are allocated to be filled by this function. Any extra entry is reserved for
+    /// platform independent data.
+    #[cfg(feature = "rme")]
+    fn rme_prepare_manifest(_buf: &mut [u8; RMM_SHARED_BUFFER_SIZE]);
 
     /// Platform dependent LogSink implementation type for Logger.
     type LogSinkImpl: LogSink;

src/platform/fvp.rs
@@ -7,7 +7,13 @@ mod config;
 use self::config::{FVP_CLUSTER_COUNT, FVP_MAX_CPUS_PER_CLUSTER, FVP_MAX_PE_PER_CPU};
 use super::{DummyService, Platform};
 #[cfg(feature = "rme")]
-use crate::Services;
+use crate::{
+    Services,
+    services::rmmd::{
+        RMM_SHARED_BUFFER_SIZE,
+        manifest::{RmmBootManifest, RmmConsoleInfo, RmmMemoryBank},
+    },
+};
 use crate::{
     aarch64::{dsb_ish, dsb_sy, wfi},
     bl31_warm_entrypoint,
@@ -24,7 +30,7 @@ use crate::{
     logger::{self, LockedWriter},
     naked_asm,
     pagetable::{
-        IdMap, MT_DEVICE, MT_MEMORY,
+        IdMap, MT_DEVICE, MT_MEMORY_EL3,
         early_pagetable::{EarlyRegion, define_early_mapping},
     },
     platform::CpuExtension,
@@ -124,7 +130,7 @@ const HW_CONFIG_ADDRESS_NS: u64 = 0x8200_0000;
 const EARLY_REGIONS: [EarlyRegion; 2] = [
     EarlyRegion {
         address_range: ARM_TRUSTED_SRAM_BASE..(ARM_TRUSTED_SRAM_BASE + ARM_TRUSTED_SRAM_SIZE),
-        attributes: MT_MEMORY,
+        attributes: MT_MEMORY_EL3,
     },
     EarlyRegion {
         address_range: UART_BASE..(UART_BASE + UART_SIZE),
@@ -185,6 +191,48 @@ unsafe impl Platform for Fvp {
 
     #[cfg(feature = "rme")]
     const RMM_SHARED_BUFFER_START: usize = 0xffbf_f000;
+
+    #[cfg(feature = "rme")]
+    fn rme_prepare_manifest(buf: &mut [u8; RMM_SHARED_BUFFER_SIZE]) {
+        use crate::services::rmmd::manifest::{
+            RMM_BOOT_MANIFEST_ROOT_COMPLEX_VERSION, RMM_BOOT_MANIFEST_VERSION,
+            RmmRootComplexInfoList,
+        };
+
+        let manifest = RmmBootManifest {
+            version: RMM_BOOT_MANIFEST_VERSION,
+            plat_data: &[],
+            plat_dram: &[
+                RmmMemoryBank {
+                    base: NT_FW_CONFIG_ADDRESS as usize,
+                    size: 0x7c00_0000,
+                },
+                RmmMemoryBank {
+                    base: *MemoryMap::DRAM1.start(),
+                    size: 0x8000_0000,
+                },
+            ],
+            plat_console: &[RmmConsoleInfo {
+                // Value from the pl011_uart crate.
+                base: UART_BASE,
+                // Values from TF-A.
+                map_pages: 0x1,
+                name: *b"pl011\0\0\0",
+                clk_in_hz: 0x00e1_0000,
+                baud_rate: 115_200,
+                flags: 0,
+            }],
+            plat_ncoh_region: &[],
+            plat_coh_region: &[],
+            plat_smmu: &[],
+            plat_root_complex: RmmRootComplexInfoList {
+                rc_info_version: RMM_BOOT_MANIFEST_ROOT_COMPLEX_VERSION,
+                entries: &[],
+            },
+        };
+
+        manifest.pack(buf, buf.as_ptr() as usize);
+    }
 
     type LogSinkImpl = LockedWriter<Uart<'static>>;
     type PsciPlatformImpl = FvpPsciPlatformImpl<'static>;

src/platform/qemu.rs
@@ -19,7 +19,7 @@ use crate::{
     },
     naked_asm,
     pagetable::{
-        IdMap, MT_DEVICE, MT_MEMORY, disable_mmu_el3,
+        IdMap, MT_DEVICE, MT_MEMORY_EL3, disable_mmu_el3,
         early_pagetable::{EarlyRegion, define_early_mapping},
     },
     platform::{CpuExtension, plat_my_core_pos},
@@ -121,7 +121,7 @@ pub struct Qemu;
 define_early_mapping!([
     EarlyRegion {
         address_range: BL31_BASE..BL32_BASE,
-        attributes: MT_MEMORY
+        attributes: MT_MEMORY_EL3
     },
     EarlyRegion {
         address_range: DEVICE1_BASE..(DEVICE1_BASE + DEVICE1_SIZE),

commit cd51ad320b01a81349a5b690a6e557bbeaa9ea36

fix(rme): improve safety requirements for `get_shared_buffer`
Update the safety requirements of `get_shared_buffer()` to take
concurrent accesses into account.

Fixes: fb2ec36 ("feat(rme): allocate boot manifest for R-EL2 payload")
Change-Id: I62e0c32ff476ace9943016fa647280775cfc10d5
Signed-off-by: Ludovic Mermod <ludovic.mermod@arm.com>

src/services/rmmd.rs
@@ -25,13 +25,12 @@ pub const RMM_SHARED_BUFFER_SIZE: usize = 0x1000;
 ///
 /// # Safety
 ///
-/// Calling this function is always safe, but using its return value is safe if all the conditions
-/// below are met:
+/// Calling this function and using its return value is safe if all the conditions below are met:
 ///
 /// - It can only be called after the shared buffer is mapped into the page table.
 /// - After calling `get_shared_buffer`, the return reference must be dropped before any other call
 ///   to it is made.
-/// - The reference must be dropped before switching to Realm World.
+/// - No PE is running in Realm World while the reference is held.
 unsafe fn get_shared_buffer() -> &'static mut [u8; RMM_SHARED_BUFFER_SIZE] {
     // Safety: (relative to [`slice::from_raw_parts_mut`][https://doc.rust-lang.org/stable/core/slice/fn.from_raw_parts_mut.html])
     // - The first condition of `get_shared_buffer()` ensures that the location is valid, and as it
@@ -154,7 +153,8 @@ impl Rmmd {
         // - This function is called after initializing the MMU and pagetable.
         // - This function never calls again `get_shared_buffer()`, thus the reference will be dropped
         //   upon return, before another call is made.
-        // - Similarly to the above, this function does not switch to the Realm World.
+        // - This function is called before the first switch to Realm world and, similarly to above,
+        //   the reference is dropped before that switch.
         let buf = unsafe { get_shared_buffer() };
         PlatformImpl::rme_prepare_manifest(buf);
         info!("RMM Boot Manifest ready");